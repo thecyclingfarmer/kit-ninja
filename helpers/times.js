d11a366748f817568899ae10bdf78d9a1393bf1a
thecyclingfarmer <martgust@gmail.com>
2015-09-14 14:28:30 +0200

    Added ninja

---
+++ helpers/times.js
@@ -0,0 +1,11 @@
+(function() {
+	module.exports.register = function(Handlebars, options) {
+		Handlebars.registerHelper('times', function (n, block) {
+			var accum = '';
+			for (var i = 0; i < n; ++i) {
+				accum += block.fn(i);
+			}
+			return accum;
+		});
+	};
+}).call(this);
