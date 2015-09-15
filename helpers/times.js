0244b900911b53078606ea731abe7231816eea44
thecyclingfarmer <martgust@gmail.com>
2015-09-15 14:02:27 +0200

    changes

--- helpers/times.js
+++ helpers/times.js
@@ -1,4 +1,11 @@
-(function() {
+module.exports = function (n, block) {
+	var accum = '';
+	for (var i = 0; i < n; ++i) {
+		accum += block.fn(i);
+	}
+	return accum;
+};
+/*(function() {
 	module.exports.register = function(Handlebars, options) {
 		Handlebars.registerHelper('times', function (n, block) {
 			var accum = '';
@@ -9,3 +16,4 @@
 		});
 	};
 }).call(this);
+*/
